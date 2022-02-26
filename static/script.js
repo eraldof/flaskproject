a62c706f0920472800167938a253e46cda2e2053
eraldof <eraldoest@gmail.com>
2022-02-26 03:21:30 -0300

    dale

--- static/script.js
+++ static/script.js
@@ -10,3 +10,16 @@ menuMobile.addEventListener("click", () => {
         : menuMobile.classList.replace("bi-x", "bi-list")
     body.classList.toggle("menu-nav-active");
 });
+
+
+/* Fecha menu e muda o icone */
+
+const navItem = document.querySelectorAll('.nav-item');
+
+navItem.forEach((item) =>{
+    item.addEventListener("click", () => {
+        if(body.classList.contains("menu-nav-active")){
+            body.classList.remove("menu-nav-active")
+            menuMobile.classList.replace("bi-x", "bi-list");}
+    })
+});
