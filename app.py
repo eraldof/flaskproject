d4a9365fb2b7e941d99eeab8d1b7c5370f7fefe7
eraldof <eraldoest@gmail.com>
2022-02-27 18:18:00 -0300

    excluindo o email

--- app.py
+++ app.py
@@ -10,11 +10,6 @@ app = Flask(__name__)
 def home():
     return render_template("homepage.html")
 
-@app.route("/contato")
-def contato():
-    return "email: eraldoest@gmail.com"
-
-
 #Colocando no ar
 if __name__ == "__main__":
     app.run(debug=True)
